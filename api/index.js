91dea5e4518fdca9e93fedf103c224851df26efa
jeel105512 <jeelhpatel10@gmail.com>
2024-06-13 12:00:17 -0400

    created test user route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import MongooseSetup from "./lib/MongooseSetup.js";
 
+import userRoutes from "./routes/user.route.js";
+
 const app = express();
 
 MongooseSetup();
@@ -8,3 +10,5 @@ MongooseSetup();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
