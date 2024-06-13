52db11313f7beff3cc5dfbc77b5d18427c8a2480
jeel105512 <jeelhpatel10@gmail.com>
2024-06-13 11:48:46 -0400

    added user model

--- api/index.js
+++ api/index.js
@@ -1,19 +1,9 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGODB)
-  .then(() => {
-    console.log("Connected to MongoDB!");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+import MongooseSetup from "./lib/MongooseSetup.js";
 
 const app = express();
+
+MongooseSetup();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
