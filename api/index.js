4401a987b377a8d790230c189a6976de0200a91d
jeel105512 <jeelhpatel10@gmail.com>
2024-06-13 11:38:37 -0400

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
-import exporess from "express";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = exporess();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
